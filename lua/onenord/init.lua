1162e1d72182b87aeb8ffa60c8fda9a7835dd622
xaaha <pratik.philos@gmail.com>
2025-09-09 13:23:54 -0400

    rm unused integrations

--- lua/onenord/init.lua
+++ lua/onenord/init.lua
@@ -1,8 +1,3 @@
--- Colorscheme name:        onenord.nvim
--- Description:             A Neovim theme that combines the Nord and Atom One Dark color palettes.
--- Author:                  Ryan Mehri
--- Website:                 https://github.com/rmehri01/onenord.nvim
-
 local config = require("onenord.config")
 local util = require("onenord.util")
 
